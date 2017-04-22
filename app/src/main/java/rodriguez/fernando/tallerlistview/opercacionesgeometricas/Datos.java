58b5b620b448ee6bae936f2724348c903c1ab9c7
Fernando Rodriguez <Fejota001@hotmail.com>
2017-04-22 16:10:03 -0500

    Inicio

---
+++ app/src/main/java/rodriguez/fernando/tallerlistview/opercacionesgeometricas/Datos.java
@@ -0,0 +1,13 @@
+package rodriguez.fernando.tallerlistview.opercacionesgeometricas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+    }
+}
